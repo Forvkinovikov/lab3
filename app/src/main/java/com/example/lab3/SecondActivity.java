7b08c2770a91c0a21d966108c56fff57b652f9ba
Stasno <54759371+Stasno@users.noreply.github.com>
2021-12-01 22:36:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab3/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        textView.setPadding(16, 16, 16, 16);
+
+        textView.setText("Лабораторная работа № 3. Работу выполнил...");
+        setContentView(textView);
+        setContentView(R.layout.activity_second);
+    }
+}
